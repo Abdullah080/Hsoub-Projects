02d898927a09c86af77268378956c7a6744ff9bb
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-05-18 15:14:26 +0400

    Most Sold Products section is completed

--- level 4/src/index.js
+++ level 4/src/index.js
@@ -4,3 +4,12 @@ import "./css/style.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((item) => new bootstrap.Tooltip(item));
+
+// Use alert when you click button
+const buttons = document.querySelectorAll(".add-to-cart-btn");
+
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", () => {
+    alert("أضيف المُنتج إلى عربة الشراء");
+  });
+});
