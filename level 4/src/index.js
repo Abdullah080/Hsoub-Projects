0522e0fa68dba390252885a3b171a0c1fa936a7c
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-06-06 15:14:36 +0400

    continue writing code for checkout page

--- level 4/src/index.js
+++ level 4/src/index.js
@@ -17,7 +17,7 @@ buttons.forEach((buttons) => {
 // Current Year
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة" + " " + new Date().getFullYear();
 
-// 
+// ---- Product ----
 document.querySelectorAll('.size-option input[type="radio"]').forEach((item) => {
   item.addEventListener("change", () => {
     document.querySelectorAll(".size-option").forEach((i) => {
@@ -32,5 +32,26 @@ document.querySelectorAll('.color-option input[type="radio"]').forEach((item) =>
       i.classList.remove("active");
     });
     item.parentNode.parentNode.classList.add("active");
+  });
+});
+
+// ---- Checkout ----
+document.querySelectorAll("[data-product-quantity]").forEach((item) => {
+  item.addEventListener("change", () => {
+    const newQuantity = item.value;
+    const parent = item.closest("[data-product-info]");
+    const pricePerUnit = parent.getAttribute("data-product-price");
+    const totalPriceForProduct = pricePerUnit * newQuantity;
+    parent.querySelector(".total-price-per-product").innerHTML = totalPriceForProduct + "$";
+
+    // Total Price
+    let totalPriceForAllProduct = 0;
+    document.querySelectorAll("[data-product-info]").forEach((product) => {
+      const pricePerUnit = product.getAttribute("data-product-price");
+      const quantity = product.querySelector("[data-product-quantity]").value;
+      const totalPriceForProduct = pricePerUnit * quantity;
+      totalPriceForAllProduct += totalPriceForProduct;
+    });
+    document.getElementById("total-price-for-all-product").innerHTML = totalPriceForAllProduct + "$";
   });
 });
